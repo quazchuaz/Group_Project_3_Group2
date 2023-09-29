1b00ef45dc348402adc336dad2ea5becb4b0d88b
quazchuaz <quazchuaz@gmail.com>
2023-09-28 20:29:04 -0400

    adjusted file structure

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 
 #################################################
 # Database Setup
@@ -31,7 +31,7 @@ quality = Base.classes.quality
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
+CORS
 
 
 #################################################
